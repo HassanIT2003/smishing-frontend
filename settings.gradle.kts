c8f630b8a425dc16ef70285b7fc1bb632655f947
HassanIT2003 <133470148+HassanIT2003@users.noreply.github.com>
2025-05-15 22:58:09 +1000

    Revert "🟡 Implement Live Smishing Radar Feature #207 (Fixed)"

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,15 +11,14 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
-
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
-        maven(url = "https://jitpack.io") // ✅ Needed for MPAndroidChart and other GitHub libraries
     }
 }
 
 rootProject.name = "SmishingDetectionApp"
 include(":app")
+ 
